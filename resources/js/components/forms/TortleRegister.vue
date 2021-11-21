3c4871ab01aa647a6e7120785414d865f45b5d7e
Mike L <mike@desertrat.io>
2021-11-21 15:19:32 +0100

    tortle-21 oops, didn't lint

--- resources/js/components/forms/TortleRegister.vue
+++ resources/js/components/forms/TortleRegister.vue
@@ -6,12 +6,19 @@
         class="flex flex-col gap-y-2 w-full mt-40"
         method="post"
     >
-        <input :value="csrf" name="_token" type="hidden">
-        <tortle-input id="tortle-name" v-model="name" class="text-2xl"
-                      input-classes="rounded-md h-16 pl-2 pr-2 w-full"
-                      label="Name: "
-                      name="name"
-                      type="text"
+        <input
+            :value="csrf"
+            name="_token"
+            type="hidden"
+        >
+        <tortle-input
+            id="tortle-name"
+            v-model="name"
+            class="text-2xl"
+            input-classes="rounded-md h-16 pl-2 pr-2 w-full"
+            label="Name: "
+            name="name"
+            type="text"
         ></tortle-input>
         <tortle-input
             id="tortle-email"
@@ -71,58 +78,58 @@
 
 <script>
 
-import TortleInput from "./controls/TortleInput";
-import TortleButton from "./controls/TortleButton";
-import Loading from "vue-loading-overlay";
-import axios from "axios";
+    import TortleInput from "./controls/TortleInput";
+    import TortleButton from "./controls/TortleButton";
+    import Loading from "vue-loading-overlay";
+    import axios from "axios";
 
-export default {
-    data() {
-        return {
-            name: "",
-            email: "",
-            password: "",
-            passwordConfirm: "",
-            isRegistering: false,
-            postTo: ""
-        };
-    },
-    components: {
-        TortleInput, TortleButton, Loading
-    },
-    methods: {
-        doRegister() {
-            this.isRegistering = true;
-            axios.post("/api/v1/user/register", {
-                name: this.name,
-                email: this.email,
-                password: this.password,
-                // for validation purposes, this is not correct naming in our standard
-                // so I've delimited it as a string property
-                "password_confirmation": this.passwordConfirm
-            }).then(response => {
-                this.isRegistering = false;
-                this.postTo = response.data.data.post_to;
-                this.$nextTick(() => this.$refs["registration-form"].submit());
-                console.log(response);
-
-            }).catch(error => {
-                this.isRegistering = false;
-                console.error(error);
-            });
+    export default {
+        data() {
+            return {
+                name: "",
+                email: "",
+                password: "",
+                passwordConfirm: "",
+                isRegistering: false,
+                postTo: ""
+            };
         },
-        reset() {
-            this.name = "";
-            this.email = "";
-            this.password = "";
-            this.passwordConfirm = "";
+        components: {
+            TortleInput, TortleButton, Loading
+        },
+        methods: {
+            doRegister() {
+                this.isRegistering = true;
+                axios.post("/api/v1/user/register", {
+                    name: this.name,
+                    email: this.email,
+                    password: this.password,
+                    // for validation purposes, this is not correct naming in our standard
+                    // so I've delimited it as a string property
+                    "password_confirmation": this.passwordConfirm
+                }).then(response => {
+                    this.isRegistering = false;
+                    this.postTo = response.data.data.post_to;
+                    this.$nextTick(() => this.$refs["registration-form"].submit());
+                    console.log(response);
+
+                }).catch(error => {
+                    this.isRegistering = false;
+                    console.error(error);
+                });
+            },
+            reset() {
+                this.name = "";
+                this.email = "";
+                this.password = "";
+                this.passwordConfirm = "";
+            }
+        },
+        props: {
+            csrf: {
+                type: String,
+                required: true
+            }
         }
-    },
-    props: {
-        csrf: {
-            type: String,
-            required: true
-        }
-    }
-};
+    };
 </script>
